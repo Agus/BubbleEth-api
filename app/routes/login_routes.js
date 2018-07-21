a54d4d8b030ee429c7fd02db039baf8ea3280dd5
Agustin <agustinmodugno@hotmail.com>
2018-07-21 20:42:20 -0300

    Adding login

--- app/routes/login_routes.js
+++ app/routes/login_routes.js
@@ -4,16 +4,25 @@ var bcrypt = require('bcryptjs');
 module.exports = function(app, db) {
 
   app.post('/login', (req, res) => {
-    var fakePassword = bcrypt.hashSync('asd')
-    var passwordIsValid = bcrypt.compareSync(req.body.password, fakePassword);
-    if (!passwordIsValid)
-      return res.status(401).send({auth: false, token: null});
-    var token = jwt.sign({
-      id: 'asd'
-    }, process.env.tokenSecret, {
-      expiresIn: 86400 // expires in 24 hours
+    const details = {
+      'email': req.body.email
+    };
+    db.collection('users').findOne(details, (err, user) => {
+      if (err) {
+        return res.send({'error': 'User does not exist :()'});
+      } else {
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        if (!passwordIsValid)
+          return res.status(401).send({auth: false, token: null});
+        var token = jwt.sign({
+          id: 'asd'
+        }, process.env.tokenSecret, {
+          expiresIn: 86400 // expires in 24 hours
+        });
+        res.status(200).send({auth: true, token: token});
+
+      }
     });
-    res.status(200).send({auth: true, token: token});
   });
 
   app.get('/logout', function(req, res) {
--- app/routes/register_routes.js
+++ app/routes/register_routes.js
@@ -5,15 +5,35 @@ var VerifyToken = require('../auth/verifyToken');
 module.exports = function(app, db) {
 
   app.post('/register', (req, res) => {
-    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-    var token = jwt.sign({
-      id: 'asd'
-    }, process.env.tokenSecret, {
-      expiresIn: 86400 // expires in 24 hours
-    });
-    res.status(201).send({auth: true, token: token})
-  })
 
+    const details = {
+      'email': req.body.email
+    };
+    db.collection('users').findOne(details, (err, item) => {
+      if (err)
+        return res.send({'error': 'Cannot create user :('});
+      if (item != undefined)
+        return res.send({'error': 'User already exists'});
+      var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+      const user = {
+        'email': req.body.email,
+        'username': req.body.username,
+        'password': hashedPassword
+      };
+      db.collection('users').insert(user, (err, result) => {
+        if (err) {
+          return res.send({'error': 'Cannot create user :('});
+        } else {
+          var token = jwt.sign({
+            id: 'asd'
+          }, process.env.tokenSecret, {
+            expiresIn: 86400 // expires in 24 hours
+          });
+          return res.status(201).send({auth: true, token: token})
+        }
+      })
+    });
+  });
   app.get('/register', VerifyToken, (req, res) => {
     res.send(200);
   });
