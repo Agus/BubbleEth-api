f11bee3a1cb10de86ac211d013fd788e281c7b6f
Agustin <agustinmodugno@hotmail.com>
2018-07-21 16:42:16 -0300

    Using .env

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/db')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+  if (process.env.dbUrl == undefined) {
+    throw 'No db url';
+  }
+}
+
 const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect(db.url, (err,database) =>{
-  if (err) return console.log(err);
+MongoClient.connect(process.env.dbUrl, (err, database) => {
+  if (err)
+    return console.log(err);
   database = database.db('bubble-dev');
-  require('./app/routes')(app,database);
+  require('./app/routes')(app, database);
 })
 
-
 app.listen(port, () => {
+  console.log('DB url: ' + process.env.dbUrl);
   console.log('We are live on ' + port);
 });
 
-/*
-Use https://medium.freecodecamp.org/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52 for token login
-*/
+/* Use https://medium.freecodecamp.org/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52 for token login */
